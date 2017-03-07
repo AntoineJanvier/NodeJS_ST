2ac090f0f90436a18dc1267640bdcbfee29f0533
AntoineJanvier <antoine.jan95@gmail.com>
2017-03-07 17:46:03 +0100

    Introduction to middlewares

--- express/e1/index.js
+++ express/e1/index.js
@@ -3,20 +3,63 @@
 * on 07/03/17.
 */
 let express = require('express');
+let cookieParser = require('cookie-parser');
 
 let app = express();
 
+// Execution du (des) middleware(s)
+app.use(function (req, res, next) {
+    console.log("work in progress...");
+    next(); // Nécessaire pour poursuivre vers le routing
+});
+app.use(cookieParser());
+app.get("/cookies", function (req, res) {
+    res.json(req.cookies);
+});
+
+// GET : Racine
 app.get("/", function (req, res) {
     res.type("html");
     res.send("<html><body>RACINE</body></html>");
-}).get("/hello/:name", function (req, res) {
+});
+// GET : Affiche bonjour avec une couleur donnée en GET
+app.get("/b", function (req, res) {
+    res.type("html");
+    res.send("<html><body style='color:" + req.query.macouleur + ";'>Bonjour</body></html>");
+});
+// GET : Affiche Hello + name
+app.get("/hello/:name", function (req, res) {
     res.json({
         "message": "Hello " + req.params.name + ' !'
     })
 });
+// GET : Répète le message donné
+app.get("/say/:msg", function (req, res) {
+    res.type('text');
+    res.send(req.params.msg);
+});
+// Test sur les cookies
+app.get("/set_cookie", function (req, res) {
+    res.cookie(req.query["key"], req.query["value"], {maxAge: 9999});
+    res.json({
+        "result": 1,
+    })
+});
+// POST : Ecoute '/post' avec le type POST
+app.post("/post", function (req, res) {
+    res.type('text');
+    res.send('Type post');
+});
+// ALL : Pour le reste, 404
+app.all('*', function (req, res) {
+    console.log('404');
+    res.status(404);
+    res.type('text');
+    res.send('-> NO WAY!');
+});
 
 app.listen(1101, function () {
-    console.log('Server started on port 1101');
+    console.log('Server started on port 1101...');
 });
 
 /**
